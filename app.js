d4c17455bb6826764028701022e7c10c16a43afd
Kaphrosh <olakunbiolabode@yahoo.com>
2021-01-01 17:56:25 +0100

    dd

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ hamburger.addEventListener("click", () => {
 
 
 
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=def445bf6e7b03a17a250c80ff2931bc&page=2'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=def445bf6e7b03a17a250c80ff2931bc&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
